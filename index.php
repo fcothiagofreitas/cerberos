941843d278ccfa743d60b169effc619efe4b0433
Thiago Freitas <fcothiagofreitas@gmail.com>
2016-02-22 12:58:42 -0300

    Primeiro UP

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta>
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Document</title>
+		<!-- Descricao do site ate 160 caracteres -->
+		<meta name="description" content="A Tapeba é um agência criativa, focada em viabilizar os projetos dos clientes de forma eficiente, efetiva e eficaz.">
+		<meta name="keywords" content="Criação, desenvolvimento, web, designer, design, sites, fortaleza, ceará"> 
+		<meta name="author" content="Tapeba Criative"> 
+
+
+		<meta property="og:type" content="website">
+		<meta property="og:locale" content="pt_BR">
+		<meta property="og:url" content="http://www.tapebacriative.com.br">
+		<meta property="og:title" content="Tapeba Criative - Agência Digital">
+		<meta property="og:site_name" content="Tapeba Criative">
+		<meta property="og:description" content="A Tapeba é um agência criativa, focada em viabilizar os projetos dos clientes de forma eficiente, efetiva e eficaz.">
+		<meta property="og:image" content="assets/imgs/img-og.png">
+		<meta property="og:image:type" content="assets/imgs/png">
+		<meta property="og:image:width" content="800"> <!-- PIXELS -->
+		<meta property="og:image:height" content="600"> <!-- PIXELS -->
+		 
+
+		<!-- CSS -->
+		<link href="assets/css/style.css" rel="stylesheet">
+		<link href="assets/css/reset.css" rel="stylesheet">
+	    <link href="assets/css/grid-bootstrap.css" rel="stylesheet">
+	    <link href="assets/css/crycle.css" rel="stylesheet">
+	    <link href="assets/css/animate.css" rel="stylesheet">
+	    <link href="assets/css/hover.css" rel="stylesheet">
+
+	</head>
+	<body>
+		<div class="content">
+			
+		</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="assets/js/jquery.cycle2.js"></script>
+	    <script src="assets/js/wow.min.js"></script>
+
+	    <script src="assets/js/main.js"></script>
+	</body>
+</html>
